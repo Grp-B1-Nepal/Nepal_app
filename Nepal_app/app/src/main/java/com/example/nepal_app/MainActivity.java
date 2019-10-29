84c278c54328ffbb0f60de638420115aaa3de2db
Gustav <gustav.nobert1998@gmail.com>
2019-10-29 13:40:51 +0100

    made the navigation bar and fragments to match

--- Nepal_app/app/src/main/java/com/example/nepal_app/MainActivity.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.nepal_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+import com.example.nepal_app.fragments.ActivitiesFragment;
+import com.example.nepal_app.fragments.HomeFragment;
+import com.example.nepal_app.fragments.ProfileFragment;
+import com.example.nepal_app.fragments.ProgressFragment;
+import com.example.nepal_app.fragments.RecipesFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
     }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_activities:
+                            openFragment(ActivitiesFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_progress:
+                            openFragment(ProgressFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_recipe:
+                            openFragment(RecipesFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
---
+++ Nepal_app/app/src/main/java/com/example/nepal_app/fragments/BottomNavigationView.java
@@ -0,0 +1,5 @@
+package com.example.nepal_app.fragments;
+
+public class BottomNavigationView {
+
+}

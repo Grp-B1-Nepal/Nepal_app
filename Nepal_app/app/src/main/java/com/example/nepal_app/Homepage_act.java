5638da48660f8b24ab38662a94cbe3ac965d641a
Jakob Mortensen <j.mortensen1996@outlook.dk>
2019-10-31 14:04:18 +0100

    created layout and added placeholder image

---
+++ Nepal_app/app/src/main/java/com/example/nepal_app/Homepage_act.java
@@ -0,0 +1,14 @@
+package com.example.nepal_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homepage_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage_layout);
+    }
+}

fb59faaba69e2b32fd071972d25bd0db88c9b8c3
Jakob Mortensen <j.mortensen1996@outlook.dk>
2019-10-31 14:15:45 +0100

    set up buttons and onclicklisteners

--- Nepal_app/app/src/main/java/com/example/nepal_app/Homepage_act.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/Homepage_act.java
@@ -3,12 +3,47 @@ package com.example.nepal_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Homepage_act extends AppCompatActivity {
+public class Homepage_act extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView recipesI, developI, activitiesI;
+    private Button recipeB, developB, activitesB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_layout);
+
+        //settin up images
+        recipesI = findViewById(R.id.imageRecipe);
+        developI = findViewById(R.id.imageDevelopment);
+        activitiesI = findViewById(R.id.imageActivities);
+
+        //setting up buttons
+        recipeB = findViewById(R.id.recipesButton);
+        developB = findViewById(R.id.developButton);
+        activitesB = findViewById(R.id.activitiesButton);
+
+        //setting up listeners
+        recipeB.setOnClickListener(this);
+        developB.setOnClickListener(this);
+        activitesB.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == recipeB) {
+            System.out.println("do nothing");
+        }
+        else if(v == developB) {
+            System.out.println("do nothing");
+        }
+        else if(v == activitesB){
+            System.out.println("do nothing");
+        }
+
     }
 }

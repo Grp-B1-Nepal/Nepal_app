86a0a1a7070cfdacdeedd704cbda96084372e5c0
PomFritzen <nicklas.fritzen@hotmail.com>
2019-11-03 11:37:08 +0100

    Correct Constraints

--- Nepal_app/app/src/main/java/com/example/nepal_app/MainActivity.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nepal_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView recipe_view = findViewById(R.id.recipe_image);
+        recipe_view.setImageResource(R.drawable.dal);
+
+
     }
 }

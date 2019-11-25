2bab12c021fbea152667b4ae3559b4831673f223
Gustav <gustav.nobert1998@gmail.com>
2019-11-25 10:22:25 +0100

    merged another branch

--- Nepal_app/app/src/main/java/com/example/nepal_app/fragments/HomeFragment.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/fragments/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.nepal_app.fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -15,7 +18,7 @@ import com.example.nepal_app.R;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,9 +32,11 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
     public HomeFragment() {
         // Required empty public constructor
     }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -49,12 +54,16 @@ public class HomeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+           if (getArguments() != null) {
+               mParam1 = getArguments().getString(ARG_PARAM1);
+               mParam2 = getArguments().getString(ARG_PARAM2);
+           }
+            rod = inflater.inflate(R.layout.homepage_layout, container, false);
+
             //settin up images
             recipesI = rod.findViewById(R.id.imageRecipe);
             developI = rod.findViewById(R.id.imageDevelopment);
@@ -81,13 +90,47 @@ public class HomeFragment extends Fragment {
             develoSound.setOnClickListener(this);
             activitySound.setOnClickListener(this);
 
-
-        }
+        return rod;
     }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void onClick(View v) {
+        if (v == recipeB) {
+            //Intent i = new Intent(this, reipes_akt.class);
+            //startActivity(i);
+        } else if (v == developB) {
+            //Intent i = new Intent(this, development_akt.class);
+            //startActivity(i);
+        } else if (v == activitesB) {
+            //Intent i = new Intent(this, activities_akt.class);
+            //startActivity(i);
+        } else if (v == recipSound) {
+            MediaPlayer mp = MediaPlayer.create(getActivity(), R.raw.meow);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        } else if (v == develoSound) {
+            MediaPlayer mp = MediaPlayer.create(getActivity(), R.raw.meow);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        } else if (v == activitySound) {
+            MediaPlayer mp = MediaPlayer.create(getActivity(), R.raw.meow);
+            mp.start();
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
     }
 }

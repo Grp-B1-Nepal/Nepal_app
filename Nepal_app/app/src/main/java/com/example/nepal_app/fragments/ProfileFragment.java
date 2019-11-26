495a9aeb2aa356e8f669ae30c5588c6df53c07d2
fredrikmejs <s185130@student.dtu.dk>
2019-11-26 02:00:29 +0100

    added errorhandling for empty fields

--- Nepal_app/app/src/main/java/com/example/nepal_app/fragments/ProfileFragment.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/fragments/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,16 +17,23 @@ import com.example.nepal_app.fragments.child.ChildObj;
 import com.example.nepal_app.fragments.child.Fragment_addChild;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
     private ListView list;
     private String[] child;
+    private String[] birthday;
+    private String[] gender;
     private FloatingActionButton add;
     private POJO pojo;
     private ArrayList<ChildObj> childArr = new ArrayList<>();
+    Spinner genders;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,12 +47,9 @@ public class ProfileFragment extends Fragment {
 
         list = view.findViewById(R.id.list);
 
-        child = new String[childArr.size()];
-        for (int i = 0; i<childArr.size();i++){
-        child[i] = childArr.get(i).getName();
-        }
+        getChild();
 
-
+        getBirthday();
 
 
         ArrayAdapter adapter = new ArrayAdapter(getContext(), R.layout.child_element, R.id.name, child);
@@ -58,6 +63,36 @@ public class ProfileFragment extends Fragment {
 
 
         return view;
+    }
+
+    private void getChild(){
+        child = new String[childArr.size()];
+        for (int i = 0; i<childArr.size();i++){
+            child[i] = childArr.get(i).getName();
+        }
+    }
+
+    //TODO fix the apdapter
+    private void getBirthday() {
+        Calendar calendar = Calendar.getInstance();
+        birthday = new String[childArr.size()];
+        String date1;
+        String date2;
+        String date3;
+        for (int i = 0; i < childArr.size(); i++) {
+            calendar.setTimeInMillis(childArr.get(i).getBirthday());
+            date1 = calendar.getTime().toString().substring(4,10);
+            date2 = calendar.getTime().toString().substring(30,34);
+            date3 =  "Birthday " + date1 + " " +  date2;
+            birthday[i] = date3;
+        }
+    }
+
+    private void getGender(){
+        gender = new String[childArr.size()];
+        for (int i = 0; i<childArr.size();i++){
+            gender[i] = childArr.get(i).getGender();
+        }
     }
 
 
--- Nepal_app/app/src/main/java/com/example/nepal_app/fragments/child/Fragment_addChild.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/fragments/child/Fragment_addChild.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,6 +14,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,14 +23,11 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.example.nepal_app.Factory.GetAndSet;
 import com.example.nepal_app.Factory.POJO;
 import com.example.nepal_app.R;
-import com.example.nepal_app.fragments.ProfileFragment;
 import com.google.gson.Gson;
 
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Objects;
@@ -46,15 +43,15 @@ public class Fragment_addChild extends Fragment implements View.OnClickListener,
     private ConstraintLayout billede;
     private ArrayList<ChildObj> childArr = new ArrayList<>();
     private long currentDate;
-
-    //Gallery select variables
-    public static final int GALLERY_SELECT = 1887;
+    private static final int PICK_IMAGE =100;
 
     //Upload variables
     String ImageString = "flexicu";
     Bitmap ImageData = null;
     Uri imageUri = null;
     POJO pojo;
+
+
     public Fragment_addChild() {
         // Required empty public constructor
     }
@@ -69,7 +66,7 @@ public class Fragment_addChild extends Fragment implements View.OnClickListener,
         name = view2.findViewById(R.id.name);
         pick_date = view2.findViewById(R.id.pickdate_button);
         gender = view2.findViewById(R.id.gender);
-        preview = view2.findViewById(R.id.imageViewPreview);
+        preview = view2.findViewById(R.id.downloaded_picture);
         preview.setVisibility(View.INVISIBLE);
 
         pick_date.setOnClickListener(this);
@@ -79,19 +76,41 @@ public class Fragment_addChild extends Fragment implements View.OnClickListener,
 
         childArr = pojo.getChildArr(getContext());
 
-        //If you don't have the permission to open the Gallery ask for them, otherwise open the gallery
+
+
         billede.setOnClickListener((view) ->{
+            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(gallery,PICK_IMAGE);
+        });
+
+        //If you don't have the permission to open the Gallery ask for them, otherwise open the gallery
+       /* billede.setOnClickListener((view) ->{
             Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
             photoPickerIntent.setType("image/*");
             startActivityForResult(photoPickerIntent, GALLERY_SELECT);
         });
-
+*/
 
 
         return view2;
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+//TODO FIND A WAY TO SAVE THE PICTURE IN A CACHE
+            if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+                imageUri = data.getData();
+                preview.setImageURI(imageUri);
+                if (imageUri != null) {
+                    preview.setVisibility(View.VISIBLE);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    /*
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Bitmap photo = null;
         if (resultCode == RESULT_OK) {
@@ -104,17 +123,26 @@ public class Fragment_addChild extends Fragment implements View.OnClickListener,
             }
             ImageData = photo;
         }
-
-
     }
+
+     */
 
     @Override
     public void onClick(View v) {
         if (v == save){
-            childArr.add(new ChildObj(String.valueOf(name.getText()),currentDate ,String.valueOf(gender.getText())));
-            saveChild();
-            FragmentManager fm = Objects.requireNonNull(getActivity()).getSupportFragmentManager();
-            fm.popBackStack();
+
+            if (String.valueOf(name.getText()).equals("")){
+                name.setError("Please fill the name of the child");
+            } else if (currentDate == 0){
+                pick_date.setError("Please pick the birthday of the child");
+            } else if (String.valueOf(gender.getText()).equals("")){
+                gender.setError("Please type the gender of the child");
+            }else {
+                childArr.add(new ChildObj(String.valueOf(name.getText()), currentDate, String.valueOf(gender.getText())));
+                saveChild();
+                FragmentManager fm = Objects.requireNonNull(getActivity()).getSupportFragmentManager();
+                fm.popBackStack();
+            }
         }
 
         if (v == pick_date) {

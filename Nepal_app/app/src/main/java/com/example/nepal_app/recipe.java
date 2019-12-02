cd9552f6365088bfb9114f4d765091da4e0e6572
fredrikmejs <s185130@student.dtu.dk>
2019-12-02 09:45:53 +0100

    removed .idea folder and changed location of the .gitignore

--- Nepal_app/app/src/main/java/com/example/nepal_app/recipe.java
+++ Nepal_app/app/src/main/java/com/example/nepal_app/recipe.java
@@ -1,12 +1,12 @@
 package com.example.nepal_app;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 

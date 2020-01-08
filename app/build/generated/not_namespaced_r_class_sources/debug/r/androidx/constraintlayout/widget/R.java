2ad2feba7a932374c4778b1e5b44c92b25cad16a
PomFritzen <nicklas.fritzen@hotmail.com>
2020-01-08 14:32:52 +0100

    recipe home layout is correct. Needs to show all recipes still. Needs cleanup

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f09004b;
-        public static final int end = 0x7f090083;
-        public static final int gone = 0x7f0900a9;
-        public static final int invisible = 0x7f0900bc;
-        public static final int left = 0x7f0900c3;
-        public static final int packed = 0x7f0900fe;
-        public static final int parent = 0x7f090100;
-        public static final int percent = 0x7f090106;
-        public static final int right = 0x7f090126;
-        public static final int spread = 0x7f090153;
-        public static final int spread_inside = 0x7f090154;
-        public static final int start = 0x7f090159;
-        public static final int top = 0x7f090182;
-        public static final int wrap = 0x7f090192;
+        public static final int end = 0x7f090081;
+        public static final int gone = 0x7f0900a7;
+        public static final int invisible = 0x7f0900b8;
+        public static final int left = 0x7f0900bf;
+        public static final int packed = 0x7f0900f9;
+        public static final int parent = 0x7f0900fb;
+        public static final int percent = 0x7f090101;
+        public static final int right = 0x7f090119;
+        public static final int spread = 0x7f090144;
+        public static final int spread_inside = 0x7f090145;
+        public static final int start = 0x7f09014a;
+        public static final int top = 0x7f090173;
+        public static final int wrap = 0x7f090184;
     }
     public static final class styleable {
         private styleable() {}

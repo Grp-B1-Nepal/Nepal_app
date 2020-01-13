b6c238c4a22bc74b475906077ff8cd4a48df54c3
Joesofine <zainamail@student.dtu.dk>
2020-01-13 09:33:40 +0100

    Progres buttons functions

--- app/src/main/java/com/example/nepal_app/UI/Fragments/Progress/ProgressFragment.java
+++ app/src/main/java/com/example/nepal_app/UI/Fragments/Progress/ProgressFragment.java
@@ -1,5 +1,6 @@
 package com.example.nepal_app.UI.Fragments.Progress;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,8 @@ public class ProgressFragment extends Fragment {
     ImageView image;
     TextView info, age, box_age, height, weight;
     ConstraintLayout box;
-    Button b1, b2, b3, b4, b5, b6, b7, b8,b9,b10,b11,b12;
+    private Button b1, b2, b3, b4, b5, b6, b7, b8,b9,b10,b11,b12;
+    private int agerange;
 
 
     @Override
@@ -25,7 +27,27 @@ public class ProgressFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_progress, container, false);
         b1 = view.findViewById(R.id.b1);
+        b2 = view.findViewById(R.id.b2);
+        b3 = view.findViewById(R.id.b3);
+        b4 = view.findViewById(R.id.b4);
+        b5 = view.findViewById(R.id.b5);
+        b6 = view.findViewById(R.id.b6);
+        b7 = view.findViewById(R.id.b7);
+        b8 = view.findViewById(R.id.b8);
+        b9 = view.findViewById(R.id.b9);
+        b10 = view.findViewById(R.id.b10);
+        b11 = view.findViewById(R.id.b11);
+        b12 = view.findViewById(R.id.b12);
 
+
+
+        if (agerange == 1) {
+            b1.getBackground().setColorFilter(getResources().getColor(R.color.redMat), PorterDuff.Mode.MULTIPLY);
+        } else if (agerange == 2) {
+            b2.getBackground().setColorFilter(getResources().getColor(R.color.redMat), PorterDuff.Mode.MULTIPLY);
+        } else if (agerange == 3) {
+            b3.getBackground().setColorFilter(getResources().getColor(R.color.redMat), PorterDuff.Mode.MULTIPLY);
+        }
 
 
         // Inflate the layout for this fragment

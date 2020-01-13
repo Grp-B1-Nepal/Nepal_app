76800c52e6db2889f907eabfe5e06eb1562e5b8e
Gustav <gustav.nobert1998@gmail.com>
2020-01-13 13:04:07 +0100

    mini commit

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f090064;
-        public static final int end = 0x7f090096;
-        public static final int gone = 0x7f0900b3;
-        public static final int invisible = 0x7f0900c6;
-        public static final int left = 0x7f0900cd;
-        public static final int packed = 0x7f090106;
-        public static final int parent = 0x7f090108;
-        public static final int percent = 0x7f09010e;
-        public static final int right = 0x7f090120;
-        public static final int spread = 0x7f09014c;
-        public static final int spread_inside = 0x7f09014d;
-        public static final int start = 0x7f090152;
-        public static final int top = 0x7f09017b;
-        public static final int wrap = 0x7f09018b;
+        public static final int end = 0x7f090095;
+        public static final int gone = 0x7f0900b2;
+        public static final int invisible = 0x7f0900c5;
+        public static final int left = 0x7f0900cc;
+        public static final int packed = 0x7f090105;
+        public static final int parent = 0x7f090107;
+        public static final int percent = 0x7f09010d;
+        public static final int right = 0x7f09011f;
+        public static final int spread = 0x7f09014b;
+        public static final int spread_inside = 0x7f09014c;
+        public static final int start = 0x7f090151;
+        public static final int top = 0x7f09017a;
+        public static final int wrap = 0x7f09018a;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/nepal_app/MainActivity.java
+++ app/src/main/java/com/example/nepal_app/MainActivity.java
@@ -16,14 +16,11 @@ import com.example.nepal_app.UI.Fragments.Recipes.RecipeHome;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         openFragment(new HomeFragment());
     }
     public void openFragment(Fragment fragment) {
@@ -31,29 +28,4 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.container, fragment);
         transaction.commit();
     }
-
-    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                    switch (item.getItemId()) {
-                        case R.id.navigation_home:
-                            openFragment(new HomeFragment());
-                            return true;
-                        case R.id.navigation_activities:
-                            openFragment(new ActivitiesFragment());
-                            return true;
-                        case R.id.navigation_profile:
-                            openFragment(new ProfileFragment());
-                            return true;
-                        case R.id.navigation_progress:
-                            openFragment(new ProgressFragment());
-                            return true;
-                        case R.id.navigation_recipe:
-                            openFragment(new RecipeHome());
-                            return true;
-                    }
-                    return false;
-                }
-            };
 } //ellers lav aktiviterer mellem de andres? ved godt vi ikke har aftalt hvordan vi gør det
